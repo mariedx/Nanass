6787e65a043c848abe1bfa440dbbccf6ff3e1ae1
Mtwod <maximewong@hotmail.com>
2021-03-17 13:19:30 +0100

    #1 Initial commit with eslint and sass

---
+++ client/src/pages/_app.jsx
@@ -0,0 +1,8 @@
+import 'styles/globals.scss';
+
+const MyApp = ({ Component, pageProps }) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <Component {...pageProps} />
+);
+
+export default MyApp;
