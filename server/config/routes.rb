dcd5290e567ddd108eba5f5998c48602ac584e9e
Mtwod <maximewong@hotmail.com>
2021-03-18 11:54:37 +0100

    #4 Add Devise User

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  default_url_options :host => "http://localhost:3000/"
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
