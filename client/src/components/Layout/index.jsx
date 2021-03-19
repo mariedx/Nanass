fd01587f798690101b8f3fc3e8beea925d36bcf4
Clementine <clementine.fouque@gmail.com>
2021-03-19 18:39:38 +0100

    layout init

--- client/src/components/Layout/index.jsx
+++ client/src/components/Layout/index.jsx
@@ -1,10 +1,16 @@
 import Navbar from 'components/Navbar';
 import styles from './layout.module.scss';
 
-const Layout = ({ children }) => (
+const Layout = ({ children, home }) => (
   <div className={styles.Layout}>
     <Navbar />
     <main>
+      {home && (
+        <h1 className="Layout__yesHome">ON EST À LA MAISON !!</h1>
+      )}
+      {!home && (
+        <h1 className="Layout__notHome">ON N&apos;EST PAS À LA MAISON !!</h1>
+      )}
       {children}
     </main>
   </div>
