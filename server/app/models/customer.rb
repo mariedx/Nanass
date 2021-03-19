65b79e34a57f9a273a26e0dbbb57662c2a5492fb
Mtwod <maximewong@hotmail.com>
2021-03-19 11:02:25 +0100

    #9 Add email to customer api response

--- server/app/models/customer.rb
+++ server/app/models/customer.rb
@@ -1,5 +1,11 @@
 class Customer < ApplicationRecord
   belongs_to :user
 
+  attribute :email, :string, default: :email
+
+  def email
+    user.email
+  end
+
   validates_presence_of :first_name, :last_name
 end
