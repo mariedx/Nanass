9ee62f921a2b5c2ccd5867bcf71cc95595fbb5de
Clementine <clementine.fouque@gmail.com>
2021-03-19 21:24:16 +0100

    testing layout on concept page

---
+++ client/src/pages/concept/index.jsx
@@ -0,0 +1,10 @@
+import Layout from 'components/Layout';
+import React from 'react';
+
+const Concept = () => (
+  <Layout title="Concept" subtitle="Bienvenue chez Nanass !">
+    <p>Hello la commu</p>
+  </Layout>
+);
+
+export default Concept;
