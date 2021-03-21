2ef72516f039ed28a468042c4a420bf161531243
Coline Brault <coline.brault@gmail.com>
2021-03-21 22:07:52 +0100

    #43 purchase page with sidebar

--- client/src/pages/concept/index.jsx
+++ client/src/pages/concept/index.jsx
@@ -1,10 +1,9 @@
-import CardMacBook from 'components/CardMacBook';
 import Layout from 'components/Layout';
 import React from 'react';
 
 const Concept = () => (
   <Layout title="Concept" subtitle="Bienvenue chez Nanass !">
-    <CardMacBook />
+    Helllllloooow
   </Layout>
 );
 
