96c053e7b265b81961e062e3416fafc8cc46f9c6
Coline Brault <coline.brault@gmail.com>
2021-03-21 19:55:06 +0100

    #43 visual effect on hover done

--- client/src/components/CardMacBook/index.jsx
+++ client/src/components/CardMacBook/index.jsx
@@ -1,11 +1,33 @@
 import styles from './cardmacbook.module.scss';
 
-const CardMacBook = () => (
+const CardMacBook = ({
+  model, size, year, processor, ram, price,
+}) => (
   <div className={styles.CardMacBook}>
-    <img src="https://i.pinimg.com/originals/20/12/b4/2012b42a4ea9a504786efaff5dab7f15.jpg" alt="MacBook" className={styles.CardMacBook__picture} />
-    <p className={styles.CardMacBook__title}>MacBook Pro 15&apos;</p>
-    <p className={styles.CardMacBook__body}>Mid 2015 - 8Go - 256Go</p>
-    <p className={styles.CardMacBook__price}>500 $</p>
+    <div className={styles.CardMacBook__picture}>
+      <img src="https://i.pinimg.com/originals/20/12/b4/2012b42a4ea9a504786efaff5dab7f15.jpg" alt="MacBook" />
+    </div>
+    <p className={styles.CardMacBook__title}>
+      {model}
+      {' '}
+      {size}
+    </p>
+    <p className={styles.CardMacBook__body}>
+      {year}
+      {' '}
+      -
+      {' '}
+      {processor}
+      {' '}
+      -
+      {' '}
+      {ram}
+    </p>
+    <p className={styles.CardMacBook__price}>
+      {price}
+      {' '}
+      $
+    </p>
   </div>
 );
 
