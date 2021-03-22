77bf94c6664485f424782ae67cdb8329301abcea
Mtwod <maximewong@hotmail.com>
2021-03-22 14:42:06 +0100

    Decomment authentication logic

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import Image from 'next/image';
 import styles from './navbar.module.scss';
 
+const currentUser = { id: null };
+
 const Navbar = () => (
   <nav className={styles.Navbar}>
     <ul>
@@ -23,46 +25,41 @@ const Navbar = () => (
     </ul>
 
     <div className={styles.Navbar__right}>
-      {/* {!currentUser.id && (
-        <> */}
-      <ul>
-        {/* <li>
-            <Link href="/log-in">
+      {!currentUser.id && (
+        <ul>
+          <li>
+            <Link href="/registrations/signin">
               <a className={styles.Navbar__link}>Connexion</a>
             </Link>
           </li>
           <li>
-            <Link href="/sign-up">
+            <Link href="/registrations/signup">
               <a className={styles.Navbar__link}>Inscription</a>
             </Link>
-
-          </li> */}
-        {/* </>
-        )} */}
-
-        {/* {currentUser.id && (
-        <> */}
-        <li>
-          <Link href="/concept">
-            <a className={styles.Navbar__link}> Notre concept</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/profil">
-            <a className={styles.Navbar__link}>Mon espace</a>
-          </Link>
-        </li>
-
-        <li>
-          <Link href="/logout">
-            <a className={styles.Navbar__link}>
-              Déconnexion
-            </a>
-          </Link>
-        </li>
-        {/* </>
-        )} */}
-      </ul>
+          </li>
+        </ul>
+      )}
+      {currentUser.id && (
+        <ul>
+          <li>
+            <Link href="/concept">
+              <a className={styles.Navbar__link}> Notre concept</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/profil">
+              <a className={styles.Navbar__link}>Mon espace</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/logout">
+              <a className={styles.Navbar__link}>
+                Déconnexion
+              </a>
+            </Link>
+          </li>
+        </ul>
+      )}
     </div>
   </nav>
 );
