ceb2262a4dfd33c5db9e5c7136fcdf5cb9ec94ba
Coline Brault <coline.brault@gmail.com>
2021-03-24 16:15:59 +0100

    #63 index page with fetch of macbook done

---
+++ client/src/api/macbooks/index.js
@@ -0,0 +1,24 @@
+import axiosInstance from 'api';
+
+class ApiMacbooks {
+  static async getAllMacbooks() {
+    try {
+      const response = await axiosInstance.get('/macbooks');
+
+      const { data } = response;
+
+      return data;
+      // return response;
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error);
+      return {
+        error: {
+          message: 'Erreur',
+        },
+      };
+    }
+  }
+}
+
+export default ApiMacbooks;
