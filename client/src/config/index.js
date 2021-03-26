f0f33cad610a9b4c1da0367ebfd1ee30bbc8f413
Clementine <clementine.fouque@gmail.com>
2021-03-26 19:37:31 +0100

    contact form working and designed

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -2,6 +2,10 @@ const config = {
   COOKIE_STORAGE_KEY_USER_TOKEN: 'Nanass--app__user-token',
   COOKIE_STORAGE_KEY_USER_DATA: 'Nanass--app__user-data',
   API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  EMAIL_JS_SERVICE: process.env.NEXT_PUBLIC_EMAIL_JS_SERVICE,
+  EMAIL_JS_TEMPLATE: process.env.NEXT_PUBLIC_EMAIL_JS_TEMPLATE,
+  EMAIL_JS_USER: process.env.NEXT_PUBLIC_EMAIL_JS_USER,
+
 };
 
 export default config;
