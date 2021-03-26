9442b771969971f6e9b013f29f931b505b6ad478
MarieDechaux <marie.dechaux@gmail.com>
2021-03-26 23:06:33 +0100

    fix admin & user achitecture and naming

--- client/src/pages/user/applications/index.jsx
+++ client/src/pages/user/applications/index.jsx
@@ -3,20 +3,20 @@ import Layout from 'components/Layout';
 import SideBar from 'components/SideBar';
 import ProgressBar from 'components/ProgressBar';
 import CardMacBook from 'components/CardMacBook';
-import styles from './application.module.scss';
+import styles from './applications.module.scss';
 
-const Application = () => (
+const Applications = () => (
   <Layout title="Mon espace">
-    <div className={styles.Application}>
-      <div className={styles.Application__sidebar}>
-        <SideBar href="./order" title="Mes commandes" />
-        <SideBar href="./application" title="Mes candidatures" />
+    <div className={styles.Applications}>
+      <div className={styles.Applications__sidebar}>
+        <SideBar href="./orders" title="Mes commandes" />
+        <SideBar href="./applications" title="Mes candidatures" />
         <SideBar href="./profile" title="Mon profil" />
       </div>
 
-      <div className={styles.Application__body}>
-        <h3 className={styles.Application__title}>Où en sont mes candidatures?</h3>
-        <div className={styles.Application__card}>
+      <div className={styles.Applications__body}>
+        <h3 className={styles.Applications__title}>Où en sont mes candidatures?</h3>
+        <div className={styles.Applications__card}>
           <CardMacBook
             model="Macbook Air"
             size="15"
@@ -26,7 +26,7 @@ const Application = () => (
             price="490"
             image="https://images.pexels.com/photos/5588490/pexels-photo-5588490.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
           />
-          <div className={styles.Application__card__progressbar}>
+          <div className={styles.Applications__card__progressbar}>
             <p>Envoyée</p>
             <ProgressBar />
             <p>Traitée</p>
@@ -38,4 +38,4 @@ const Application = () => (
   </Layout>
 );
 
-export default Application;
+export default Applications;
