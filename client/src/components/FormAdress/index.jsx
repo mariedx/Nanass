368318c6c6dae88274e76ac7847379faab02f7d1
Clementine <clementine.fouque@gmail.com>
2021-03-26 11:13:59 +0100

    delivery form init

---
+++ client/src/components/FormAdress/index.jsx
@@ -0,0 +1,71 @@
+import Input from 'components/RegisInput';
+import { useState } from 'react';
+import styles from './formadress.module.scss';
+
+const FormAdress = () => {
+  const [firstNameValue, setFirstNameValue] = useState('');
+  const [lastNameValue, setLastNameValue] = useState('');
+  const [streetNumberValue, setStreetNumberValue] = useState('');
+  const [streetNameValue, setStreetNameValue] = useState('');
+  const [zipValue, setZipValue] = useState('');
+  const [countryValue, setCountryValue] = useState('');
+  const [phoneValue, setPhoneValue] = useState('');
+  const [complementValue, setComplementValue] = useState('');
+
+  return (
+    <div className={styles.FormAdress}>
+      <h3>Indiquez votre adresse de livraison</h3>
+      <Input
+        title="Prénom"
+        type="text"
+        handleChange={setFirstNameValue}
+        value={firstNameValue}
+      />
+      <Input
+        title="Nom"
+        type="text"
+        handleChange={lastNameValue}
+        value={setLastNameValue}
+      />
+      <Input
+        title="N°"
+        type="text"
+        handleChange={setStreetNumberValue}
+        value={streetNumberValue}
+      />
+      <Input
+        title="Rue"
+        type="text"
+        handleChange={setStreetNameValue}
+        value={streetNameValue}
+      />
+      <Input
+        title="Code postal"
+        type="text"
+        handleChange={setZipValue}
+        value={zipValue}
+      />
+      <Input
+        title="Pays"
+        type="text"
+        handleChange={countryValue}
+        value={setCountryValue}
+      />
+      <Input
+        title="Téléphone"
+        type="text"
+        handleChange={phoneValue}
+        value={setPhoneValue}
+      />
+      <Input
+        title="Compléments"
+        type=""
+        handleChange={complementValue}
+        value={setComplementValue}
+        placeholder="Digicode, indication supplémentaire..."
+      />
+    </div>
+  );
+};
+
+export default FormAdress;
