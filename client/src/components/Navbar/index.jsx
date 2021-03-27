a583ae6765dd0a31391ae6b3a9d2491a922f00a8
Mtwod <maximewong@hotmail.com>
2021-03-27 20:53:13 +0100

    Add redirection to home after logout

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -8,6 +8,7 @@ import { logout } from 'store/users/userActions';
 import ApiSessions from 'api/sessions';
 import Cookies from 'js-cookie';
 import config from 'config';
+import { useRouter } from 'next/router';
 
 const Nav = styled.nav`
   padding: 20px 50px 20px 0px;
@@ -117,12 +118,14 @@ const Navbar = () => {
   const [toggle, toggleNav] = useState(false);
   const currentUser = useSelector((state) => state);
   const dispatch = useDispatch();
+  const router = useRouter();
 
   const handleLogout = async (event) => {
     event.preventDefault();
     const token = Cookies.get(config.COOKIE_STORAGE_KEY_USER_TOKEN);
     await ApiSessions.logout({ token });
     dispatch(logout());
+    router.push('/');
   };
 
   return (
