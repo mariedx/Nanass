8fed01b51da9d9569f5211cf9538a8b85bd16c83
Clementine <clementine.fouque@gmail.com>
2021-03-27 12:20:22 +0100

    jumbotron ok

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -4,6 +4,14 @@ import Layout from 'components/Layout';
 const Home = () => (
   <Layout home>
     <Jumbotron />
+    <style jsx global>
+      {`
+        body {
+background-color: var(--color-secondary);
+background: linear-gradient(to bottom, #ffff 50%, var(--color-secondary) 50%);
+        }
+      `}
+    </style>
   </Layout>
 );
 
