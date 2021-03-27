cfdfa14dbd28fbf2f34328c975bff38b6b316238
Clementine <clementine.fouque@gmail.com>
2021-03-27 16:41:59 +0100

    fix page

--- client/src/pages/concept/index.jsx
+++ client/src/pages/concept/index.jsx
@@ -8,7 +8,7 @@ const Concept = () => (
     <div className={styles.Concept}>
       <img className={styles.Concept__image} alt="nanass - illustration" src="https://images.unsplash.com/photo-1506203396712-45cde6442eaa?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80" />
       <div className={styles.Concept__text}>
-        <span className={styles.Concept__title}> Pourquoi Nanass ?</span>
+        <h2 className={styles.Concept__title}> Pourquoi Nanass ?</h2>
         <p>
           Nanass, c&apos;est d&apos;abord deux nanas, expertes en ordinateurs Mac,
           qui veulent les réutiliser les rendre accessibles.
@@ -16,21 +16,21 @@ const Concept = () => (
           <br />
           Notre objectif ? régénérer les pommes à l&apos;infini tel un ananas !
         </p>
-        <span className={styles.Concept__title}>Responsable</span>
+        <p className={styles.Concept__title}>Responsable</p>
         <p>
           Chaque année, des millions de Mac sont vendus à travvers le monde.
           Chez Nanass, nous nous sommes fixés une mission :
           réparer un produit existant et le remettre en état, plutôt qu&apos;en produire un nouveau.
           La planète, ainsi que votre budget, vous remercierons !
         </p>
-        <span className={styles.Concept__title}>Local</span>
+        <p className={styles.Concept__title}>Local</p>
         <p>
-          Chez Nanass, nous sommes persuadés que près des yeux = près du coeur !
+          Chez Nanass, nous sommes persuadés que près des yeux, près du coeur !
           C&apos;est pourquoi nous travaillons avec des réparateurs situés en Ile-deFrance,
           à proximité de nos locaux. Nous mettons tout en oeuvre pour limiter
           au maximum les deplacements inutils et centraliser tous nos prestataires.
         </p>
-        <span className={styles.Concept__title}>Confiance</span>
+        <p className={styles.Concept__title}>Confiance</p>
         <p>
           Tous nos MacBooks sont chouchoutés de leur reception dans nos locaux à leur envoi chez vous.
           Nous inspectons minutieusement le produit dès sa reception et le confions entre les mains
