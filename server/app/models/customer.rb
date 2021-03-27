433eb422627300075faf10d251e08bab098fec80
Mtwod <maximewong@hotmail.com>
2021-03-27 18:40:45 +0100

    Add unicity to customer and admin model

--- server/app/models/customer.rb
+++ server/app/models/customer.rb
@@ -2,11 +2,21 @@ class Customer < ApplicationRecord
   belongs_to :user
   has_many :addresses
 
+  validates_presence_of :first_name, :last_name
+  before_create :abort_if_already_customer
+
   attribute :email, :string, default: :email
 
   def email
     user.email
   end
 
-  validates_presence_of :first_name, :last_name
+  private
+
+  def abort_if_already_customer
+    if user.customer?
+      errors.add(:user, 'the user is already a customer')
+      throw(:abort)
+    end
+  end
 end
